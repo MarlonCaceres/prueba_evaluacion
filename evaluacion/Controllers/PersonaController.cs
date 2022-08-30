570a0caa0c843e770910781387b9bd0dc5840d8b
itziatech <m_ricky3021@hotmail.com>
2022-08-30 18:51:23 -0500

    Evaluacion

---
+++ evaluacion/Controllers/PersonaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using evaluacion.Model;
+
+namespace evaluacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpPost("Lista")]
+        public async Task<IActionResult> Lista(ListaPersonaModel persona)
+        {
+            try
+            {
+                if (persona.ListaPersona == null)
+                    return Ok("La lista no puede ser nula.");
+
+                if (persona.ListaPersona.Count <= 0)
+                    return Ok("La lista no contiene ningún registro.");
+
+                List<string> ListaPersona = new List<string>();
+
+                foreach (var item in persona.ListaPersona)
+                {
+                    if (item.Sexo.Equals("F"))
+                        if (item.Edad>=18)
+                            ListaPersona.Add(item.Nombre + " " + item.Apellido);
+                }
+
+                return Ok(ListaPersona);
+            }
+            catch (Exception err)
+            {
+                return Ok(err.Message.ToString());
+            }
+
+        }
+    }
+}
+
---
+++ evaluacion/Model/PersonaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace evaluacion.Model
+{
+    public class PersonaModel
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+    }
+
+    public class ListaPersonaModel
+    {
+        public List<PersonaModel> ListaPersona { get; set; }
+    }
+}
+
